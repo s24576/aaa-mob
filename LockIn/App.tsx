dd09796e0aa5f8be3c0fe507e4c65a3d5cb127a8
Mikołaj Sławnikowski <mikolaj.slawnikowski@gmail.com>
2024-09-30 19:27:25 +0200

    Dodanie TailwindCSS/Nativewind do projektu

--- LockIn/App.tsx
+++ LockIn/App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 justify-center items-center bg-gray-300">
+      <Text className="text-2xl font-bold">
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
