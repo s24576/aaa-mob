8623bcd348829c187bde0bb61ac122248ac8147e
s24576 <s24576@pjwstk.edu.pl>
2024-10-04 14:16:19 +0200

    XD

--- LockIn/App.tsx
+++ LockIn/App.tsx
@@ -16,6 +16,8 @@ import { initI18n } from "./translations/i18n";
 
 initI18n();
 
+//https://49f5-194-181-181-5.ngrok-free.app
+
 type RootStackParamList = {
   Login: undefined;
   Register: undefined;
