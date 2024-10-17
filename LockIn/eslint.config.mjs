c5b30bfb021eed2a4f1864b75519b89ba38fa9fc
boguss8 <s25867@pjwstk.edu.pl>
2024-10-17 18:34:09 +0200

    lint dziala

---
+++ LockIn/eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+
+  // Add Prettier plugin
+  prettierPlugin.configs.recommended,
+
+  // Disable ESLint rules that conflict with Prettier
+  prettierConfig,
+];
