63255259cfaa43831f426fc1c1598d13f131f1f7
Mikołaj Sławnikowski <mikolaj.slawnikowski@gmail.com>
2024-12-19 18:52:39 +0100

    Usuwanie znajomych

--- LockIn/components/FriendListPage.tsx
+++ LockIn/components/FriendListPage.tsx
@@ -4,6 +4,7 @@ import { UserContext } from '../context/UserContext'
 import { useIsFocused, useNavigation } from '@react-navigation/native'
 import { UserContextType } from '../types/local/userContext'
 import { getUserData } from '../api/user/getUserData'
+import { deleteFriend } from '../api/profile/deleteFriend'
 import { ProfileScreenProps } from '../App'
 import { useSocket } from '../context/SocketProvider'
 
@@ -57,6 +58,18 @@ const FriendListPage = () => {
     navigation.navigate('LockInProfile', { username: usernameToSend })
   }
 
+  const handleDeleteFriend = async (friendId: string) => {
+    try {
+      await deleteFriend(friendId)
+      setUserData({
+        ...userData,
+        friends: userData.friends.filter((friend) => friend._id !== friendId),
+      })
+    } catch (error) {
+      console.error('Error deleting friend:', error)
+    }
+  }
+
   return (
     <View className="p-5">
       <Button
@@ -68,9 +81,12 @@ const FriendListPage = () => {
         data={userData.friends}
         keyExtractor={(item) => item._id}
         renderItem={({ item }) => (
-          <Text onPress={() => handleFriendRequest(item)}>
-            {item.username} ({item.username2})
-          </Text>
+          <View>
+            <Text onPress={() => handleFriendRequest(item)}>
+              {item.username} ({item.username2})
+            </Text>
+            <Button title="Usuń" onPress={() => handleDeleteFriend(item._id)} />
+          </View>
         )}
       />
     </View>
