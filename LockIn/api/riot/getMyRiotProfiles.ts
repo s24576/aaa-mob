49fe325721065b87684ba38f21b05374316a7d50
Mikołaj Sławnikowski <mikolaj.slawnikowski@gmail.com>
2025-01-09 20:17:05 +0100

    Dodawanie ogłoszeń o duo

--- LockIn/api/riot/getMyRiotProfiles.ts
+++ LockIn/api/riot/getMyRiotProfiles.ts
@@ -4,6 +4,7 @@ import { handleError } from '../error/handleError'
 export const getMyRiotProfiles = async () => {
   try {
     const response = await api.get(`/riot/getMyRiotProfiles`)
+    console.log('My Riot profiles:', response.data)
     return response.data
   } catch (error) {
     handleError(error)
--- LockIn/components/AnnouncementsPage.tsx
+++ LockIn/components/AnnouncementsPage.tsx
@@ -1,6 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, FlatList, StyleSheet } from 'react-native'
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Button,
+  Modal,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+} from 'react-native'
 import { getDuos } from '../api/duo/getDuos'
+import { createDuo } from '../api/duo/createDuo'
+import { getMyRiotProfiles } from '../api/riot/getMyRiotProfiles'
+import { getChampionNames } from '../api/ddragon/getChampionNames'
+import { getVersion } from '../api/ddragon/version'
 
 interface Announcement {
   _id: string
@@ -17,6 +31,34 @@ interface Announcement {
 
 const AnnouncementsPage = () => {
   const [announcements, setAnnouncements] = useState<Announcement[]>([])
+  const [modalVisible, setModalVisible] = useState(false)
+  const [pickerModal, setPickerModal] = useState({
+    visible: false,
+    type: '',
+    options: [],
+    onSelect: (value: string[]) => {},
+    multiSelect: false,
+    selectedValues: [] as string[],
+  })
+  const [newDuo, setNewDuo] = useState({
+    puuid: '',
+    positions: [],
+    lookedPositions: [],
+    minRank: '',
+    maxRank: '',
+    languages: [],
+    championIds: [],
+  })
+  const [riotProfiles, setRiotProfiles] = useState<
+    { puuid: string; gameName: string; server: string }[]
+  >([])
+  const [selectedProfile, setSelectedProfile] = useState<{
+    puuid: string
+    gameName: string
+  } | null>(null)
+  const [champions, setChampions] = useState<{ [key: string]: string }>({})
+  const [selectedChampions, setSelectedChampions] = useState<string[]>([])
+  const [version, setVersion] = useState<string>('')
 
   useEffect(() => {
     const fetchAnnouncements = async () => {
@@ -32,8 +74,77 @@ const AnnouncementsPage = () => {
     fetchAnnouncements()
   }, [])
 
+  useEffect(() => {
+    const fetchRiotProfiles = async () => {
+      try {
+        const profiles = await getMyRiotProfiles()
+        setRiotProfiles(profiles)
+      } catch (error) {
+        console.error('Error fetching Riot profiles:', error)
+      }
+    }
+
+    fetchRiotProfiles()
+  }, [])
+
+  useEffect(() => {
+    const fetchChampions = async () => {
+      try {
+        const data = await getChampionNames()
+        setChampions(data)
+      } catch (error) {
+        console.error('Error fetching champions:', error)
+      }
+    }
+
+    fetchChampions()
+  }, [])
+
+  useEffect(() => {
+    const fetchVersion = async () => {
+      try {
+        const data = await getVersion()
+        setVersion(data)
+      } catch (error) {
+        console.error('Error fetching version:', error)
+      }
+    }
+
+    fetchVersion()
+  }, [])
+
+  const handleCreateDuo = async () => {
+    try {
+      const response = await createDuo(newDuo)
+      console.log('Duo created successfully:', response)
+      setModalVisible(false)
+      // Optionally, refresh the announcements list
+      const data = await getDuos({}, {})
+      setAnnouncements(data.content)
+    } catch (error) {
+      console.error('Error creating duo:', error)
+    }
+  }
+
+  const openPickerModal = (
+    type: string,
+    options: string[],
+    onSelect: (value: string[]) => void,
+    multiSelect: boolean = false,
+    selectedValues: string[] = []
+  ) => {
+    setPickerModal({
+      visible: true,
+      type,
+      options,
+      onSelect,
+      multiSelect,
+      selectedValues,
+    })
+  }
+
   const renderAnnouncement = ({ item }: { item: Announcement }) => (
-    <View style={styles.announcement}>
+    <View className="border 1px solid black my-2">
       <Text>Author: {item.author}</Text>
       <Text>Server: {item.server}</Text>
       <Text>
@@ -43,20 +154,277 @@ const AnnouncementsPage = () => {
   )
 
   return (
-    <FlatList
-      data={announcements}
-      keyExtractor={(item) => item._id}
-      renderItem={renderAnnouncement}
-      ListEmptyComponent={<Text>No announcements found.</Text>}
-    />
+    <View>
+      <Button title="Add announcement" onPress={() => setModalVisible(true)} />
+      <FlatList
+        data={announcements}
+        keyExtractor={(item) => item._id}
+        renderItem={renderAnnouncement}
+        ListEmptyComponent={<Text>No announcements found.</Text>}
+      />
+      <Modal visible={modalVisible} animationType="slide">
+        <View>
+          <Text className="my-2">Add New Duo</Text>
+
+          <View className="my-2">
+            <Text>Select Riot Account</Text>
+            <Button
+              title={
+                selectedProfile ? selectedProfile.gameName : 'Select Account'
+              }
+              onPress={() =>
+                openPickerModal(
+                  'Select Riot Account',
+                  riotProfiles.map((profile) => profile.gameName),
+                  (values) => {
+                    const profile = riotProfiles.find(
+                      (profile) => profile.gameName === values[0]
+                    )
+                    if (profile) {
+                      setSelectedProfile(profile)
+                      setNewDuo({
+                        ...newDuo,
+                        puuid: `${profile.server}_${profile.puuid}`,
+                      })
+                    }
+                  }
+                )
+              }
+            />
+          </View>
+
+          <View className="my-2">
+            <Text>Your position</Text>
+            <Button
+              title={newDuo.positions.join(', ')}
+              onPress={() =>
+                openPickerModal(
+                  'Your position',
+                  ['Top', 'Jungle', 'Mid', 'Bot', 'Support', 'Fill'],
+                  (values) => setNewDuo({ ...newDuo, positions: values }),
+                  true,
+                  newDuo.positions
+                )
+              }
+            />
+          </View>
+
+          <View className="my-2">
+            <Text>Searched position</Text>
+            <Button
+              title={newDuo.lookedPositions.join(', ')}
+              onPress={() =>
+                openPickerModal(
+                  'Searched position',
+                  ['Top', 'Jungle', 'Mid', 'Bot', 'Support', 'Fill'],
+                  (values) => setNewDuo({ ...newDuo, lookedPositions: values }),
+                  true,
+                  newDuo.lookedPositions
+                )
+              }
+            />
+          </View>
+
+          <View className="my-2">
+            <Text>Min rank</Text>
+            <Button
+              title={newDuo.minRank}
+              onPress={() =>
+                openPickerModal(
+                  'Min rank',
+                  [
+                    'Challenger',
+                    'Grandmaster',
+                    'Master',
+                    'Diamond',
+                    'Emerald',
+                    'Platinum',
+                    'Gold',
+                    'Silver',
+                    'Bronze',
+                    'Iron',
+                  ],
+                  (values) => setNewDuo({ ...newDuo, minRank: values[0] })
+                )
+              }
+            />
+          </View>
+
+          <View className="my-2">
+            <Text>Max rank</Text>
+            <Button
+              title={newDuo.maxRank}
+              onPress={() =>
+                openPickerModal(
+                  'Max rank',
+                  [
+                    'Challenger',
+                    'Grandmaster',
+                    'Master',
+                    'Diamond',
+                    'Emerald',
+                    'Platinum',
+                    'Gold',
+                    'Silver',
+                    'Bronze',
+                    'Iron',
+                  ],
+                  (values) => setNewDuo({ ...newDuo, maxRank: values[0] })
+                )
+              }
+            />
+          </View>
+
+          <View className="my-2">
+            <Text>Languages</Text>
+            <Button
+              title={newDuo.languages.join(', ')}
+              onPress={() =>
+                openPickerModal(
+                  'Languages',
+                  [
+                    'English',
+                    'German',
+                    'French',
+                    'Spanish',
+                    'Polish',
+                    'Chinese',
+                    'Japanese',
+                    'Korean',
+                    'Other',
+                  ],
+                  (values) => setNewDuo({ ...newDuo, languages: values }),
+                  true,
+                  newDuo.languages
+                )
+              }
+            />
+          </View>
+
+          <View className="my-2">
+            <Text>Champions</Text>
+            <Button
+              title={
+                selectedChampions.length > 0
+                  ? selectedChampions
+                      .map((champ) => champions[champ])
+                      .join(', ')
+                  : 'Select Champions'
+              }
+              onPress={() =>
+                openPickerModal(
+                  'Select Champions',
+                  Object.keys(champions),
+                  (values) => {
+                    setSelectedChampions(values)
+                    setNewDuo({ ...newDuo, championIds: values })
+                  },
+                  true,
+                  selectedChampions
+                )
+              }
+            />
+          </View>
+
+          <View className="mb-2">
+            <Button title="Create Duo" onPress={handleCreateDuo} />
+          </View>
+          <View>
+            <Button title="Cancel" onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
+
+      <Modal visible={pickerModal.visible} animationType="fade" transparent>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>{pickerModal.type}</Text>
+            <ScrollView>
+              {pickerModal.options.map((option) => (
+                <TouchableOpacity
+                  key={option}
+                  onPress={() => {
+                    if (pickerModal.multiSelect) {
+                      const selectedValues =
+                        pickerModal.selectedValues.includes(option)
+                          ? pickerModal.selectedValues.filter(
+                              (val) => val !== option
+                            )
+                          : [...pickerModal.selectedValues, option]
+                      setPickerModal({ ...pickerModal, selectedValues })
+                    } else {
+                      pickerModal.onSelect([option])
+                      setPickerModal({ ...pickerModal, visible: false })
+                    }
+                  }}
+                >
+                  <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    {pickerModal.type === 'Select Champions' ? (
+                      <Image
+                        source={{
+                          uri: `https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${option}.png`,
+                        }}
+                        style={{ width: 20, height: 20, marginRight: 10 }}
+                      />
+                    ) : null}
+                    <Text
+                      style={[
+                        styles.option,
+                        pickerModal.selectedValues.includes(option) &&
+                          styles.selectedOption,
+                      ]}
+                    >
+                      {pickerModal.type === 'Select Champions'
+                        ? champions[option]
+                        : option}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              ))}
+              {pickerModal.multiSelect && (
+                <Button
+                  title="Done"
+                  onPress={() => {
+                    pickerModal.onSelect(pickerModal.selectedValues)
+                    setPickerModal({ ...pickerModal, visible: false })
+                  }}
+                />
+              )}
+              <Button
+                title="Back"
+                onPress={() =>
+                  setPickerModal({ ...pickerModal, visible: false })
+                }
+              />
+            </ScrollView>
+          </View>
+        </View>
+      </Modal>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  announcement: {
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    width: '80%',
+  },
+  option: {
     padding: 10,
+    fontSize: 16,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
+  },
+  selectedOption: {
+    backgroundColor: '#ddd',
   },
 })
 
