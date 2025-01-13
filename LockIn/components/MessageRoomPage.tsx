3471b9d1ec21c0c229f9e7854300b303c5fc5067
boguss8 <s25867@pjwstk.edu.pl>
2025-01-13 15:12:07 +0100

    Revert "naprawa apk1"
    This reverts commit 0f66f0a4c7d96d24f617fec25ebab2ade94bb677.

--- LockIn/components/MessageRoomPage.tsx
+++ LockIn/components/MessageRoomPage.tsx
@@ -37,7 +37,7 @@ const ChatPage: React.FC = () => {
   const { userData } = useContext(UserContext) as UserContextType
   const [replyingTo, setReplyingTo] = useState<Message | null>(null)
   const [pages, setPages] = useState<number[]>([0])
-  const size = 8
+  const size = 10
   const isFirstLoad = useRef(true)
   const [isInitialDataLoaded, setIsInitialDataLoaded] = useState(false)
   const [allMessages, setAllMessages] = useState<Message[]>([])
@@ -76,8 +76,9 @@ const ChatPage: React.FC = () => {
   const sendMessageMutation = useMutation({
     mutationFn: (message: MessageProp) => sendMessage(chatId, message),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['messages', chatId] })
       setNewMessage('')
+      queryClient.invalidateQueries({ queryKey: ['messages', chatId] })
+      queryClient.invalidateQueries({ queryKey: ['chat', chatId] })
     },
   })
 
--- LockIn/components/NotificationPage.tsx
+++ LockIn/components/NotificationPage.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react'
 import { View, Text, FlatList, TouchableOpacity, Alert } from 'react-native'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
@@ -38,121 +37,10 @@ const NotificationComponent: React.FC = () => {
   if (error) {
     return <Text>Error: {error.message}</Text>
   }
-=======
-import React, { useState, useEffect } from 'react'
-import { View, Text, ScrollView } from 'react-native'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { useSocket } from '../context/SocketProvider'
-
-interface Notification {
-  type: string
-  description: string
-}
-
-const NotificationComponent: React.FC = () => {
-  const { receivedMessage, connectionStatus, messengerMessage, memberEvent } =
-    useSocket()
-  const [notifications, setNotifications] = useState<Notification[]>([])
-
-  useEffect(() => {
-    const loadNotifications = async () => {
-      try {
-        const storedNotifications = await AsyncStorage.getItem('notifications')
-        if (storedNotifications) {
-          setNotifications(JSON.parse(storedNotifications))
-        }
-      } catch (error) {
-        console.error('Failed to load notifications from storage:', error)
-      }
-    }
-
-    loadNotifications()
-  }, [])
->>>>>>> parent of 0e02280 (poczatek powiadomien)
-
-  useEffect(() => {
-    const saveNotifications = async (newNotifications: Notification[]) => {
-      try {
-        await AsyncStorage.setItem(
-          'notifications',
-          JSON.stringify(newNotifications)
-        )
-      } catch (error) {
-        console.error('Failed to save notifications to storage:', error)
-      }
-    }
-
-    if (receivedMessage) {
-      setNotifications((prevNotifications) => {
-        const updatedNotifications = [
-          { type: 'Notification', description: receivedMessage },
-          ...prevNotifications,
-        ].slice(0, 20)
-        saveNotifications(updatedNotifications)
-        return updatedNotifications
-      })
-    }
-  }, [receivedMessage])
-
-  useEffect(() => {
-    const saveNotifications = async (newNotifications: Notification[]) => {
-      try {
-        await AsyncStorage.setItem(
-          'notifications',
-          JSON.stringify(newNotifications)
-        )
-      } catch (error) {
-        console.error('Failed to save notifications to storage:', error)
-      }
-    }
-
-    if (messengerMessage) {
-      const messageData = JSON.parse(messengerMessage)
-      setNotifications((prevNotifications) => {
-        const updatedNotifications = [
-          {
-            type: 'Messenger',
-            description: `User: ${messageData.userId}, Message: ${messageData.message}`,
-          },
-          ...prevNotifications,
-        ].slice(0, 20)
-        saveNotifications(updatedNotifications)
-        return updatedNotifications
-      })
-    }
-  }, [messengerMessage])
-
-  useEffect(() => {
-    const saveNotifications = async (newNotifications: Notification[]) => {
-      try {
-        await AsyncStorage.setItem(
-          'notifications',
-          JSON.stringify(newNotifications)
-        )
-      } catch (error) {
-        console.error('Failed to save notifications to storage:', error)
-      }
-    }
-
-    if (memberEvent) {
-      setNotifications((prevNotifications) => {
-        const updatedNotifications = [
-          { type: 'Member Event', description: memberEvent },
-          ...prevNotifications,
-        ].slice(0, 20)
-        saveNotifications(updatedNotifications)
-        return updatedNotifications
-      })
-    }
-  }, [memberEvent])
 
   return (
     <View className="p-5">
-      <Text className="text-lg mb-3">
-        Connection Status: {connectionStatus}
-      </Text>
       <Text className="text-lg mb-3">Notifications:</Text>
-<<<<<<< HEAD
       <FlatList
         data={notifications?.content}
         keyExtractor={(item) => item._id}
@@ -165,16 +53,6 @@ const NotificationComponent: React.FC = () => {
           </TouchableOpacity>
         )}
       />
-=======
-      <ScrollView>
-        {notifications.map((notification, index) => (
-          <View key={index} className="p-3 my-2 border border-gray-300 rounded">
-            <Text className="font-bold">{notification.type}</Text>
-            <Text>{notification.description}</Text>
-          </View>
-        ))}
-      </ScrollView>
->>>>>>> parent of 0e02280 (poczatek powiadomien)
     </View>
   )
 }
